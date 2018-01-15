c8de6b63a3e6147cafc067aee15a258b7ed852cb
imjog <imjog24@gmail.com>
2018-01-16 02:52:52 +0530

    Added constraints to login button

--- Social Logins iOS/ViewController.swift
+++ Social Logins iOS/ViewController.swift
@@ -15,9 +15,19 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initialUISetups()
+    }
+    
+    // MARK: - Initial UI Setups
+    func initialUISetups() {
         view.addSubview(fbLoginButton)
         fbLoginButton.center = view.center
         fbLoginButton.delegate = self
+        // Add Constraints to button
+        fbLoginButton.translatesAutoresizingMaskIntoConstraints = false
+        fbLoginButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 32.0).isActive = true
+        fbLoginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32.0).isActive = true
+        fbLoginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32.0).isActive = true
     }
 
 }
